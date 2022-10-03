ccfc5c3f3f3cd44962ef4e1e9b13844ce2894fb4
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-03 07:05:50 -0400

    Handling POST request to '/new' URL

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,11 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Messageboard", messages });
 });
 
+router.post("/new", function (req, res, next) {
+  const messageUser = req.body.messageUser;
+  const messageText = req.body.messageText;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/");
+});
+
 module.exports = router;
